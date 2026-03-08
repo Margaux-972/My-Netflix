e9f15c2c87a5513759155d5abe4d162e26a7683d
Margaux <margaux.ems@gmail.com>
2026-03-08 16:27:44 +0100

    added indexes

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,13 +4,14 @@ const Section = () => {
   //   console.log(data); [{category: 'Reprendre avec', images: Array(27)}]
   return (
     <main>
-      {data.map((element) => {
+      {data.map((element, index) => {
+        // rajout clé index sur ce data map à vérifier
         //   console.log(element); {category: 'Reprendre avec', images: Array(27)}
         // console.log(element.images);
         let carrousel = element.images;
         return (
           <>
-            <h2>{element.category}</h2>
+            <h2 key={index}>{element.category}</h2>
             <div>
               {carrousel.map((pic, index) => {
                 return <img key={index} src={pic} alt="bla" />;
